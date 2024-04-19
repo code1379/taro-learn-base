646c069b3932ad7d64f3c0dd7059aa6832e3a4a4
code1379 <coderxialuo@gmail.com>
2024-04-19 17:16:15 +0800

    feat: 使用 taro create --name xxx 创建页面,以及使用tabBar

--- src/app.config.js
+++ src/app.config.js
@@ -1,10 +1,48 @@
 // https://docs.taro.zone/docs/app-config
 export default defineAppConfig({
-  pages: ["pages/style-taro/index", "pages/cpns/cpns", "pages/index/index"],
+  pages: [
+    "pages/home/index",
+    "pages/category/index",
+    "pages/cart/index",
+    "pages/profile/index",
+    "pages/style-taro/index",
+    "pages/cpns/cpns",
+    "pages/index/index",
+  ],
   window: {
     backgroundTextStyle: "light",
     navigationBarBackgroundColor: "#ff454e",
     navigationBarTitleText: "Taro App",
     navigationBarTextStyle: "white",
+  },
+  tabBar: {
+    color: "#999",
+    selectedColor: "#ff464e",
+    list: [
+      {
+        text: "首页",
+        pagePath: "pages/home/index",
+        iconPath: "assets/tabbar/home.png",
+        selectedIconPath: "assets/tabbar/home_active.png",
+      },
+      {
+        text: "分类",
+        pagePath: "pages/category/index",
+        iconPath: "assets/tabbar/category.png",
+        selectedIconPath: "assets/tabbar/category_active.png",
+      },
+      {
+        text: "购物车",
+        pagePath: "pages/cart/index",
+        iconPath: "assets/tabbar/cart.png",
+        selectedIconPath: "assets/tabbar/cart_active.png",
+      },
+      {
+        text: "我的",
+        pagePath: "pages/profile/index",
+        iconPath: "assets/tabbar/profile.png",
+        selectedIconPath: "assets/tabbar/profile_active.png",
+      },
+    ],
   },
 });
